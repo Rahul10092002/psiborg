1a7b46259f93de23247a4df1db5590f544337eb7
Rahul Patidar <117348590+Rahul10092002@users.noreply.github.com>
2025-10-25 11:04:56 +0530

    Update allowed origin for CORS to use the correct Vercel URL

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://exquisite-eclair-850129.netlify.app",
+  "https://psiborg.vercel.app",
 ];
 
 app.use(cors({
