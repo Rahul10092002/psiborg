c2edda83ba15e3bf19389e56334c833ff4d7de39
Rahul Patidar <117348590+Rahul10092002@users.noreply.github.com>
2025-10-25 10:54:47 +0530

    Update allowed origin for CORS to use the correct Netlify URL

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://psiborg.vercel.app",
+  "https://exquisite-eclair-850129.netlify.app",
 ];
 
 app.use(cors({
